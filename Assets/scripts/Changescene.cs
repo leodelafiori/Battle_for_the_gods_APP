9de341faaee5d7082987d104fc59499b48de2410
leodelafiori <leodelafiori@users.noreply.github.com>
2018-04-28 22:48:28 -0300

    App initial commit

---
+++ Assets/scripts/Changescene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Changescene : MonoBehaviour {
+
+	public Button mudacena;
+
+	// Use this for initialization
+	void Start () {
+
+		Button btn = mudacena.GetComponent<Button> ();
+		btn.onClick.AddListener (Taskclick);
+
+	}
+
+	void Taskclick()
+	{
+		SceneManager.LoadScene ("menuapp", LoadSceneMode.Single);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+
+
+}
---
+++ Assets/scripts/Display_resources_gold.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display_resources_gold : MonoBehaviour {
+
+	public Text OuroTexto;
+	public int Ouro=200;
+	public Button IniciaTurno;
+	public Button Subtrai_1;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		Button btn = IniciaTurno;
+		btn.onClick.AddListener(TaskClick);
+
+		Button btnS1 = Subtrai_1;
+		btnS1.onClick.AddListener(Subtrair1);
+
+
+		OuroTexto.text = Ouro.ToString ();
+
+	}
+
+	//Botao clicar
+	void TaskClick(){
+
+		Ouro = Ouro + 20;
+
+	}
+
+
+	void Subtrair1() {
+
+		Ouro = Ouro - 200;
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		OuroTexto.text = Ouro.ToString ();
+
+	}
+
+
+
+
+}
